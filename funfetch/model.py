f244f66357484045c1cd4ad578904f7be8d8e4e7
decave27 <decave27@gmail.com>
2021-06-19 22:12:05 +0900

    feat: create compileoutout, anytype model

--- funfetch/model.py
+++ funfetch/model.py
@@ -13,3 +13,24 @@ class Response:
 
     def __len__(self):
         return len(self._data)
+
+
+class AnyType:
+    """Some Compile format string"""
+
+    pass
+
+
+class CompileOutput:
+    def __init__(
+        self, source: str, error=False, error_type=None, error_msg=None, result=None
+    ):
+        self.source = source
+        self.result = None
+        self.error_msg = None
+        self.error = error
+        if not error:
+            self.result = result
+        else:
+            self.error_msg = error_msg
+            self.error_type = error_type
