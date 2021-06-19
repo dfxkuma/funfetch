767a0d53f8bc195ec6606ccda728c45b70609298
decave27 <decave27@gmail.com>
2021-06-19 22:15:40 +0900

    feat: create complile ast util

---
+++ funfetch/utils.py
@@ -0,0 +1,14 @@
+import ast
+
+
+def insert_returns(body):
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
